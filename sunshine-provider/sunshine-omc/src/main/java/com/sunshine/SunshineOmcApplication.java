ebcbe91fe062a76359e025bdffc9fd749ecaa220
cpazstido <120964417@qq.com>
2019-01-09 15:39:48 +0800

    添加拦截器

--- sunshine-provider/sunshine-omc/src/main/java/com/sunshine/SunshineOmcApplication.java
+++ sunshine-provider/sunshine-omc/src/main/java/com/sunshine/SunshineOmcApplication.java
@@ -1,17 +1,9 @@
 package com.sunshine;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.endpoint.AbstractEndpoint;
-import org.springframework.boot.actuate.endpoint.Endpoint;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @EnableFeignClients
 @EnableEurekaClient
