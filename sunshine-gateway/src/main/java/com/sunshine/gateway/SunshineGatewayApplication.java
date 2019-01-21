a23d34ae3c1d2bd22962efbc1e189943ea3d419a
cpazstido <120964417@qq.com>
2019-01-21 17:27:00 +0800

    spring cloud config

--- sunshine-gateway/src/main/java/com/sunshine/gateway/SunshineGatewayApplication.java
+++ sunshine-gateway/src/main/java/com/sunshine/gateway/SunshineGatewayApplication.java
@@ -3,15 +3,14 @@ package com.sunshine.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-//@EnableHystrix
 @EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
-//@EnableOAuth2Sso
 public class SunshineGatewayApplication {
 
     public static void main(String[] args) {
